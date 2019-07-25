f65b5640af580a63654737e3bd5b8c18111d6c2b
tomtidswell <tomtidswell@gmail.com>
2019-07-25 09:04:18 +0100

    front end now calling database

--- seed.py
+++ seed.py
@@ -10,30 +10,34 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-
-    cat_shopping = TransCategory(category='Shopping')
-    cat_supermarket = TransCategory(category='Supermarket')
-    cat_bill = TransCategory(category='Bill')
-    cat_mortgage = TransCategory(category='Mortgage Payment')
-
+    categories = dict(
+        shopping=TransCategory(category='Shopping'),
+        supermarket=TransCategory(category='Supermarket'),
+        eating_out=TransCategory(category='Eating Out'),
+        bill=TransCategory(category='Bill'),
+        mortgage=TransCategory(category='Mortgage Payment')
+    )
 
     accounts = (
         Account(type='Current', owner=users[0]),
         Account(type='Current', owner=users[1]),
-        Account(type='Current', owner=users[1])
+        Account(type='Current', owner=users[1]),
+        Account(type='Credit Card', owner=users[1]),
+        Account(type='Mortgage', owner=users[1])
     )
 
     anon_account = Account(type='Current')
 
     transaction1 = Transaction(
-        description='Tesco supermarket', date='2019-07-22', amount=12.99, account=accounts[0], categories=[cat_shopping, cat_supermarket]
+        description='Tesco supermarket', date='2019-07-22', amount=12.99, account=accounts[1], categories=[categories['shopping'], categories['supermarket']]
     )
     transaction2 = Transaction(
-        description='o2 Mobile Direct Debit', date='2019-07-20', amount=24.99, account=accounts[0], categories=[cat_bill]
+        description='o2 Mobile Direct Debit', date='2019-07-20', amount=24.99, account=accounts[1], categories=[categories['bill']]
     )
 
     # categories (only need to add categories that havent been used yet)
-    db.session.add(cat_mortgage)
+    for category in categories.values():
+        db.session.add(category)
 
     # users - add_all to add a list of users
     db.session.add_all(users)
--- src/components/common/Banking.js
+++ src/components/common/Banking.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import axios from 'axios'
 // import { Link } from 'react-router-dom'
 
 
 class Home extends React.Component {
+  constructor() {
+    super()
+
+    this.state = { userId: 2, currentTab: 0 }
+  }
+
+  componentDidMount() {
+    let userData = null
+    let accountData = null
+    axios.get(`/api/users/${this.state.userId}`)
+      .then(res => {
+        userData = res.data
+        // console.log(userData.accounts[0].id)
+        if(userData.accounts.length)
+          return axios.get(`/api/accounts/${userData.accounts[0].id}`)
+      })
+      .then(res => {
+        accountData = res ? res.data : null
+        this.setState({ userData, accountData })
+      })
+      .catch(err => console.log(err))
+  }
+
+  switchTab(accountId){
+    console.log('switching to', accountId)
+    axios.get(`/api/accounts/${accountId}`)
+      .then(res => this.setState({ accountData: res.data }))
+      .catch(err => console.log(err))
+  }
+
   render() {
+    console.log('user:',this.state.userData)
+    console.log('account:',this.state.accountData)
+    if(!this.state.userData || !this.state.accountData)
+      return null
+    const {userData, accountData} = this.state
+
     return (
       <section className="banking-page">
         <div className="hero bank-head">
@@ -11,18 +48,16 @@ class Home extends React.Component {
         </div>
 
         <ul className="tab tab-block">
-          <li className="tab-item">
-            <a className="">Current Account 1</a>
-          </li>
-          <li className="tab-item">
-            <a className="active">Current Account 2</a>
-          </li>
-          <li className="tab-item">
-            <a className="">Credit Card</a>
-          </li>
-          <li className="tab-item">
-            <a className="">Mortgage</a>
-          </li>
+
+          {userData.accounts.map((account, index) => (
+            <li
+              onClick={()=>this.switchTab(account.id)}
+              className={`tab-item ${index === 0 ? 'active' : '' }`}
+              key={index}>
+              <a className="">{account.type}</a>
+            </li>
+          ))}
+
         </ul>
 
         <div>
@@ -30,6 +65,7 @@ class Home extends React.Component {
           <table className="table table-hover">
             <thead>
               <tr>
+                <th>Date</th>
                 <th>Description</th>
                 <th>In</th>
                 <th>Out</th>
@@ -38,41 +74,24 @@ class Home extends React.Component {
               </tr>
             </thead>
             <tbody>
-              <tr className="">
-                <td>Tesco shop</td>
-                <td>£4.87</td>
-                <td>-</td>
-                <td>-£10.00</td>
-                <td>Shopping, Supermarket</td>
-              </tr>
-              <tr className="active">
-                <td>Tesco shop</td>
-                <td>£4.87</td>
-                <td>-</td>
-                <td>-£10.00</td>
-                <td>Shopping, Supermarket</td>
-              </tr>
-              <tr className="">
-                <td>Tesco shop</td>
-                <td>£4.87</td>
-                <td>-</td>
-                <td>-£10.00</td>
-                <td>Shopping, Supermarket</td>
-              </tr>
-              <tr className="">
-                <td>Tesco shop</td>
-                <td>£4.87</td>
-                <td>-</td>
-                <td>-£10.00</td>
-                <td>
-                  <span className="chip">
-                    Shopping <a href="#" className="btn btn-clear" aria-label="Close" role="button"></a>
-                  </span>
-                  <span className="chip">
-                    Supermarket <a href="#" className="btn btn-clear" aria-label="Close" role="button"></a>
-                  </span>
-                </td>
-              </tr>
+              {accountData.transactions.map((transaction, index) => (
+                <tr key={index}>
+                  <td>{transaction.date}</td>
+                  <td>{transaction.description}</td>
+                  <td>£{transaction.amount}</td>
+                  <td>£{transaction.amount}</td>
+                  <td>£</td>
+                  <td>
+                    {console.log(transaction.categories)}
+                    {transaction.categories.map((category, index) => (
+                      <span className={`chip is-${category.category.toLowerCase()}`} key={index}>
+                        {category.category} <a href="#" className="btn btn-clear" aria-label="Close" role="button"></a>
+                      </span>
+
+                    ))}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
